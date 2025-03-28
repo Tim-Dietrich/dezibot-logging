def075e526ee4889326e48600d438cd5467819b4
FelixHerrling <felixherrling.business@gmail.com>
2025-03-28 12:20:39 +0100

    cleaned up main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,9 @@
 #include <Arduino.h>
 #include <Dezibot.h>
-#include <cstdlib>
-#include <vector>
-#include <logger/LogDatabase.h>
-#include <logger/Logger.h>
 
 Dezibot dezibot;
 
 int i = 0;
-
-void processAllLogs() {
-    const auto& logs = LogDatabase::getInstance().getLogs();
-
-    for (const auto& log : logs) {
-        if (log.level != LogEntry::Level::TRACE) {
-            // Construct a string representation of the log entry
-            std::string logEntry = "[" + std::to_string(log.level) + "] " + log.timestamp + ": " + log.message;
-
-            // Call the placeholder function with the constructed log string
-            dezibot.display.println(logEntry.c_str());
-        }
-    }
-}
 
 void setup() {
     dezibot.begin();
